7839afc86cd7a8776e533d2d6f94c2be12715c31
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2024-02-01 18:51:55 -0600

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    if (word.length === 0) {
+        return "";
+    }
+    let result = "";
+    for(let i = 0; i < word.length; i++) {
+        result += word.charAt(word.length-1-i);
+    }
+    return result;
 };
 
 // Do not edit below this line
